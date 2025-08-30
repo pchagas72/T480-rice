0029adf764e9c3c28a623aefa717d8848ae59980
Pedro Chagas Silva <pedro.pcsilva@ufpe.br>
2025-08-30 12:31:00 -0300

    Added neovim plugins for embedded systems, and modified waybar configuration

---
+++ config/nvim/lua/plugins/esp32.lua
@@ -0,0 +1,59 @@
+return {
+ "Aietes/esp32.nvim",
+ name = "esp32.nvim",
+ dependencies = {
+  "folke/snacks.nvim",
+ },
+ opts = {
+  build_dir = "build.clang",
+ },
+ config = function(_, opts)
+  require("esp32").setup(opts)
+ end,
+ keys = {
+  {
+   "<leader>RM",
+   function()
+    require("esp32").pick("monitor")
+   end,
+   desc = "ESP32: Pick & Monitor",
+  },
+  {
+   "<leader>Rm",
+   function()
+    require("esp32").command("monitor")
+   end,
+   desc = "ESP32: Monitor",
+  },
+  {
+   "<leader>RF",
+   function()
+    require("esp32").pick("flash")
+   end,
+   desc = "ESP32: Pick & Flash",
+  },
+  {
+   "<leader>Rf",
+   function()
+    require("esp32").command("flash")
+   end,
+   desc = "ESP32: Flash",
+  },
+  {
+   "<leader>Rc",
+   function()
+    require("esp32").command("menuconfig")
+   end,
+   desc = "ESP32: Configure",
+  },
+  {
+   "<leader>RC",
+   function()
+    require("esp32").command("clean")
+   end,
+   desc = "ESP32: Clean",
+  },
+  { "<leader>Rr", ":ESPReconfigure<CR>", desc = "ESP32: Reconfigure project" },
+  { "<leader>Ri", ":ESPInfo<CR>", desc = "ESP32: Project Info" },
+ },
+}
