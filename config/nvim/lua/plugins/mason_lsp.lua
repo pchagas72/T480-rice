18735ebf0bd0bbc6c9073d5342fedbdb19f62abe
Pedro Chagas Silva <pedro.pcsilva@ufpe.br>
2025-08-31 14:10:28 -0300

    Edited zshrc and neovim configs

--- config/nvim/lua/plugins/mason_lsp.lua
+++ config/nvim/lua/plugins/mason_lsp.lua
@@ -44,6 +44,8 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
+                    "rust_analyzer",
+                    "clangd",
                 },
             handlers = {
                 function(server_name) -- default handler (optional)
