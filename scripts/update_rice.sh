d7834e183bca6379ebdb77c3aa41677be016bccb
Pedro Chagas <pedro.pcsilva@ufpe.br>
2026-02-15 11:28:07 -0300

    Removed duplicates

--- scripts/update_rice.sh
+++
@@ -1,7 +0,0 @@
-cp -r $HOME/.config/nvim $HOME/git/T480-rice/config/
-cp -r $HOME/.config/waybar $HOME/git/T480-rice/config/
-cp -r $HOME/.config/hypr $HOME/git/T480-rice/config/
-cp -r $HOME/.config/wofi $HOME/git/T480-rice/config/
-cp -r $HOME/.config/eww $HOME/git/T480-rice/config/
-cp -r $HOME/.local/bin/* $HOME/git/T480-rice/scripts/
-rm $HOME/git/T480-rice/scripts/eww
--- scripts/update_wallpaper.sh
+++
@@ -1,2 +0,0 @@
-pkill -9 wallpaper_rotat
-$HOME/.config/hypr/scripts/wallpaper_rotate.sh & disown
